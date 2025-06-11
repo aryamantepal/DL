46aa0cab8379b9b1958337c3cba0225339f30185
aryamantepal <aryamandora@gmail.com>
2025-06-11 14:09:34 +0200

    ran first exp

--- pytorch-mlflow/base.py
+++ pytorch-mlflow/base.py
@@ -29,3 +29,51 @@ lr.fit(X_train, y_train)
 y_pred = lr.predict(X_test)
 
 accuracy = accuracy_score(y_test, y_pred)
+
+
+## log model & metadata to MLflow
+
+# set up tracking server uri for logging
+mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
+
+# create a new mlflow experiment
+mlflow.set_experiment("MLflow Quickstart")
+
+with mlflow.start_run():
+    # log the hyperparameters
+    mlflow.log_params(params)
+    
+    # log the loss metric
+    mlflow.log_metric("accuracy", accuracy)
+    
+    # infer the model signature
+    signature = infer_signature(X_train, lr.predict(X_train))
+    
+    # log the model, which inherits the parameters and metric
+    model_info = mlflow.sklearn.log_model(
+        sk_model=lr,
+        name="iris_model",
+        signature = signature,
+        input_example=X_train,
+        registered_model_name="tutorial for basic model"
+    )
+    
+    # set a tag to remind us what we used the model for
+    
+    mlflow.set_logged_model_tags(
+        model_info.model_id, {"Training Info": "Basic LR model for iris data"}
+    )
+    
+    
+# we can perform inference by using MLflow's pyfunc flavor to load the model. Then, we run predict on new data using the loaded model
+load_model = mlflow.pyfunc.load_model(model_info.model_uri)
+
+predictions = load_model.predict(X_test)
+
+iris_feature_names = datasets.load_iris().feature_names
+
+result = pd.DataFrame(X_test, columns=iris_feature_names)
+result['actual_class'] = y_test
+result['predicted_class'] = predictions
+
+print(result[:4])
