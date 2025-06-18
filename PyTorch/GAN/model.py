adc0587a11f9a3ea8b42e9ea8d7189a07fb827db
aryamantepal <aryamandora@gmail.com>
2025-06-19 00:27:32 +0200

    setup

---
+++ PyTorch/GAN/model.py
@@ -0,0 +1,29 @@
+import os
+import numpy as np
+import math
+
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+cuda = True if torch.cuda.is_available() else False
+
+
+# define the generator
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        
+        def block(in_feat, out_feat, normalize=True):
+            layers = [nn.Linear(in_feat, out_feat)]
+            if normalize:
+                layers.append(nn.BatchNorm1d(out_feat, 0.8))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
