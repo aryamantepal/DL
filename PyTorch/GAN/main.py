360bad9725fbbce835967422e27d95348779ea02
Aryaman Tepal <83552886+aryamantepal@users.noreply.github.com>
2025-07-31 16:38:18 +0200

    code runs

--- PyTorch/GAN/main.py
+++ PyTorch/GAN/main.py
@@ -89,5 +89,5 @@ for epoch in range(opt.n_epochs):
         )
 
         batches_done = epoch * len(dataloader) + i
-        if batches_done % opt.sample_interval == 0:
-            save_image(gen_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
+        # if batches_done % opt.sample_interval == 0:
+        #     save_image(gen_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
