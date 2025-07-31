e4a9d627c77e411d88cb4bab2dec88e210b028e1
Aryaman Tepal <83552886+aryamantepal@users.noreply.github.com>
2025-07-31 16:19:04 +0200

    Update model.py

--- PyTorch/GAN/model.py
+++ PyTorch/GAN/model.py
@@ -34,5 +34,21 @@ class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
 
+        self.inp = nn.Linear(100, 256 * 4 * 4)
+
+        self.model = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+
     def forward(self, x):
         pass
