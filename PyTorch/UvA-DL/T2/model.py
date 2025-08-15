54721b3a5b093e0196b028557e09d9d776d7aaa2
aryamantepal <aryamandora@gmail.com>
2025-08-15 12:54:44 +0400

    fix

--- PyTorch/UvA-DL/T2/model.py
+++ PyTorch/UvA-DL/T2/model.py
@@ -1,37 +1,17 @@
-import torch.utils.data as data
-import torch
+import torch.nn as nn
 
-class XORDataset(data.Dataset):
-    def __init__(self, size, std=0.1):
-        """
-        Inputs:
-            size - Number of data points we want to generate
-            std - Standard deviation of the noise (see generate_continuous_xor function)
-        """
+class SimpleClassifier(nn.Module):
+
+    def __init__(self, num_inputs, num_hidden, num_outputs):
         super().__init__()
-        self.size = size
-        self.std = std
-        self.generate_continuous_xor()
+        # Initialize the modules we need to build the network
+        self.linear1 = nn.Linear(num_inputs, num_hidden)
+        self.act_fn = nn.Tanh()
+        self.linear2 = nn.Linear(num_hidden, num_outputs)
 
-    def generate_continuous_xor(self):
-        # Each data point in the XOR dataset has two variables, x and y, that can be either 0 or 1
-        # The label is their XOR combination, i.e. 1 if only x or only y is 1 while the other is 0.
-        # If x=y, the label is 0.
-        data = torch.randint(low=0, high=2, size=(self.size, 2), dtype=torch.float32)
-        label = (data.sum(dim=1) == 1).to(torch.long)
-        # To make it slightly more challenging, we add a bit of gaussian noise to the data points.
-        data += self.std * torch.randn(data.shape)
-
-        self.data = data
-        self.label = label
-
-    def __len__(self):
-        # Number of data point we have. Alternatively self.data.shape[0], or self.label.shape[0]
-        return self.size
-
-    def __getitem__(self, idx):
-        # Return the idx-th data point of the dataset
-        # If we have multiple things to return (data point and label), we can return them as tuple
-        data_point = self.data[idx]
-        data_label = self.label[idx]
-        return data_point, data_label
+    def forward(self, x):
+        # Perform the calculation of the model to determine the prediction
+        x = self.linear1(x)
+        x = self.act_fn(x)
+        x = self.linear2(x)
+        return x
