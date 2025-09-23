6bf348c8932d389f35364ba67898a102f879e339
aryamantepal <aryamandora@gmail.com>
2025-09-23 10:24:03 -0400

    added paths and device

--- PyTorch/UvA-DL/T3/model.py
+++ PyTorch/UvA-DL/T3/model.py
@@ -22,3 +22,25 @@ import torch.nn.functional as F
 import torch.utils.data as data
 import torch.optim as optim
 
+# Path to the folder where the datasets are/should be downloaded (e.g. MNIST)
+DATASET_PATH = "../data"
+# Path to the folder where the pretrained models are saved
+CHECKPOINT_PATH = "../saved_models/tutorial3"
+
+# Function for setting the seed
+def set_seed(seed):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available(): # GPU operation have separate seed
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+set_seed(42)
+
+# Additionally, some operations on a GPU are implemented stochastic for efficiency
+# We want to ensure that all operations are deterministic on GPU (if used) for reproducibility
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+# Fetching the device that will be used throughout this notebook
+device = torch.device("cpu") if not torch.cuda.is_available() else torch.device("cuda:0")
+print("Using device", device)
