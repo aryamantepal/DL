128f94ca4c9ef6e8e7b5745fca31f8c566fac629
aryamantepal <aryamandora@gmail.com>
2025-09-23 10:30:39 -0400

    relu variations

--- PyTorch/UvA-DL/T3/model.py
+++ PyTorch/UvA-DL/T3/model.py
@@ -66,7 +66,6 @@ for file_name in pretrained_files:
             
             
 class ActivationFunction(nn.Module):
-
     def __init__(self):
         super().__init__()
         self.name = self.__class__.__name__
@@ -80,3 +79,23 @@ class Tanh(ActivationFunction):
     def forward(self, x):
         x_exp, neg_x_exp = torch.exp(x), torch.exp(-x)
         return (x_exp - neg_x_exp) / (x_exp + neg_x_exp)
+
+class ReLU(ActivationFunction):
+    def forward(self, x):
+        return x * (x > 0).float()
+    
+class LeakyReLU(ActivationFunction):
+    def __init__(self, alpha=0.1):
+        super().__init__()
+        self.config["alpha"] = alpha
+
+    def forward(self, x):
+        return torch.where(x > 0, x, self.config["alpha"] * x)
+
+class ELU(ActivationFunction):
+    def forward(self, x):
+        return torch.where(x > 0, x, torch.exp(x)-1)
+
+class Swish(ActivationFunction):
+    def forward(self, x):
+        return x * torch.sigmoid(x)
