63b76c574305ae0318d962a12273026bb599b550
aryamantepal <aryamandora@gmail.com>
2025-10-08 00:37:47 -0400

    initialized DenseNet, added to dic

--- PyTorch/UvA-DL/T5/model.py
+++ PyTorch/UvA-DL/T5/model.py
@@ -507,3 +507,70 @@ class TransitionLayer(nn.Module):
 
     def forward(self, x):
         return self.transition(x)
+
+class DenseNet(nn.Module):
+
+    def __init__(self, num_classes=10, num_layers=[6,6,6,6], bn_size=2, growth_rate=16, act_fn_name="relu", **kwargs):
+        super().__init__()
+        self.hparams = SimpleNamespace(num_classes=num_classes,
+                                       num_layers=num_layers,
+                                       bn_size=bn_size,
+                                       growth_rate=growth_rate,
+                                       act_fn_name=act_fn_name,
+                                       act_fn=act_fn_by_name[act_fn_name])
+        self._create_network()
+        self._init_params()
+
+    def _create_network(self):
+        c_hidden = self.hparams.growth_rate * self.hparams.bn_size # The start number of hidden channels
+
+        # A first convolution on the original image to scale up the channel size
+        self.input_net = nn.Sequential(
+            nn.Conv2d(3, c_hidden, kernel_size=3, padding=1) # No batch norm or activation function as done inside the Dense layers
+        )
+
+        # Creating the dense blocks, eventually including transition layers
+        blocks = []
+        for block_idx, num_layers in enumerate(self.hparams.num_layers):
+            blocks.append(
+                DenseBlock(c_in=c_hidden,
+                           num_layers=num_layers,
+                           bn_size=self.hparams.bn_size,
+                           growth_rate=self.hparams.growth_rate,
+                           act_fn=self.hparams.act_fn)
+            )
+            c_hidden = c_hidden + num_layers * self.hparams.growth_rate # Overall output of the dense block
+            if block_idx < len(self.hparams.num_layers)-1: # Don't apply transition layer on last block
+                blocks.append(
+                    TransitionLayer(c_in=c_hidden,
+                                    c_out=c_hidden // 2,
+                                    act_fn=self.hparams.act_fn))
+                c_hidden = c_hidden // 2
+
+        self.blocks = nn.Sequential(*blocks)
+
+        # Mapping to classification output
+        self.output_net = nn.Sequential(
+            nn.BatchNorm2d(c_hidden), # The features have not passed a non-linearity until here.
+            self.hparams.act_fn(),
+            nn.AdaptiveAvgPool2d((1,1)),
+            nn.Flatten(),
+            nn.Linear(c_hidden, self.hparams.num_classes)
+        )
+
+    def _init_params(self):
+        # Based on our discussion in Tutorial 4, we should initialize the convolutions according to the activation function
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, nonlinearity=self.hparams.act_fn_name)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.input_net(x)
+        x = self.blocks(x)
+        x = self.output_net(x)
+        return x
+
+model_dict["DenseNet"] = DenseNet
