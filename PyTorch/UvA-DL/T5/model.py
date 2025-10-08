2aaa7d7043eba43ae6cd8b743bbc610116d7f739
aryamantepal <aryamandora@gmail.com>
2025-10-08 00:37:11 -0400

    denseNet

--- PyTorch/UvA-DL/T5/model.py
+++ PyTorch/UvA-DL/T5/model.py
@@ -436,3 +436,74 @@ resnetpreact_model, resnetpreact_results = train_model(model_name="ResNet",
                                                                           "momentum": 0.9,
                                                                           "weight_decay": 1e-4},
                                                        save_name="ResNetPreAct")
+
+
+# DenseNet -> DenseLayer, DenseBlock & TransitionLayer
+
+class DenseLayer(nn.Module):
+
+    def __init__(self, c_in, bn_size, growth_rate, act_fn):
+        """
+        Inputs:
+            c_in - Number of input channels
+            bn_size - Bottleneck size (factor of growth rate) for the output of the 1x1 convolution. Typically between 2 and 4.
+            growth_rate - Number of output channels of the 3x3 convolution
+            act_fn - Activation class constructor (e.g. nn.ReLU)
+        """
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.BatchNorm2d(c_in),
+            act_fn(),
+            nn.Conv2d(c_in, bn_size * growth_rate, kernel_size=1, bias=False),
+            nn.BatchNorm2d(bn_size * growth_rate),
+            act_fn(),
+            nn.Conv2d(bn_size * growth_rate, growth_rate, kernel_size=3, padding=1, bias=False)
+        )
+
+    def forward(self, x):
+        out = self.net(x)
+        out = torch.cat([out, x], dim=1)
+        return out
+    
+# The module DenseBlock summarizes multiple dense layers applied in sequence. 
+# Each dense layer takes as input the original input concatenated with all previous layers’ feature maps:
+
+class DenseBlock(nn.Module):
+
+    def __init__(self, c_in, num_layers, bn_size, growth_rate, act_fn):
+        """
+        Inputs:
+            c_in - Number of input channels
+            num_layers - Number of dense layers to apply in the block
+            bn_size - Bottleneck size to use in the dense layers
+            growth_rate - Growth rate to use in the dense layers
+            act_fn - Activation function to use in the dense layers
+        """
+        super().__init__()
+        layers = []
+        for layer_idx in range(num_layers):
+            layers.append(
+                DenseLayer(c_in=c_in + layer_idx * growth_rate, # Input channels are original plus the feature maps from previous layers
+                           bn_size=bn_size,
+                           growth_rate=growth_rate,
+                           act_fn=act_fn)
+            )
+        self.block = nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.block(x)
+        return out
+    
+class TransitionLayer(nn.Module):
+
+    def __init__(self, c_in, c_out, act_fn):
+        super().__init__()
+        self.transition = nn.Sequential(
+            nn.BatchNorm2d(c_in),
+            act_fn(),
+            nn.Conv2d(c_in, c_out, kernel_size=1, bias=False),
+            nn.AvgPool2d(kernel_size=2, stride=2) # Average the output for each 2x2 pixel group
+        )
+
+    def forward(self, x):
+        return self.transition(x)
